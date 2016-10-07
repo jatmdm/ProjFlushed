995edf1b45bc2b0941a2d12bcad3ed335f52b501
Damon Streat <jatmdm@gmail.com>
2016-10-07 14:54:50 -0400

    New branch created. Story manager started.

--- Assets/scripts/dj/StoryManager.cs
+++ Assets/scripts/dj/StoryManager.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+
+enum ConditionState { NotSeen = 0, NotChosen = 1, Chosen = 2};
 
 public class StoryManager : MonoBehaviour {
+	Dictionary<string, int> storyConditions;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void LoadStory(){
+		
+	}
+	void SaveStory(){
+		//Top of the file is "metadata"
+		for (int i = 0; i < storyConditions.Count; i++) {
+			//Put each condition as CONDITION:STATE
+		}
 	}
 }
