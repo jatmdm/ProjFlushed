7958129edc365b3b6de5de25d0dc65479b32f0ac
jatmdm <jatmdm@gmail.com>
2016-10-10 22:24:40 -0400

    Working on saving

--- Assets/scripts/dj/StoryManager.cs
+++ Assets/scripts/dj/StoryManager.cs
@@ -9,10 +9,12 @@ enum ConditionState { NotSeen = 0, NotChosen = 1, Chosen = 2};
 public class StoryManager : MonoBehaviour {
 	public Dictionary<string, int> storyConditions;
 	public bool load;
+    public bool save;
 
 	// Use this for initialization
 	void Start () {
 		load = false;
+        save = false;
 		storyConditions = new Dictionary<string, int> ();
 	}
 	
@@ -22,6 +24,23 @@ public class StoryManager : MonoBehaviour {
 			load = false;
 			LoadStory ();
 		}
+        if (save)
+        {
+            save = false;
+
+            //test whether or not you can change something and load it.
+            //I believe this is the only way to manipulate values in a Dictionary, but I will have to look some stuff up later.
+            foreach(KeyValuePair<string, int> kv in storyConditions)
+            {
+                string key;
+                int value = 0;
+
+                key = kv.Key;
+                storyConditions.Remove(kv.Key);
+                storyConditions.Add(key, value);
+            }
+            SaveStory();
+        }
 	}
 
 	void printStory(){
@@ -45,9 +64,10 @@ public class StoryManager : MonoBehaviour {
 			value = int.Parse(line.Split (new char[] {':'})[1]);
 			storyConditions.Add (key, value);
 		}
+        reader.Close();
         endTime = Time.timeSinceLevelLoad;
         Debug.Log("STORY LOADED IN " + (startTime - endTime) + "ms");
-		//printStory ();
+		printStory ();
 	}
 		
 	void SaveStory(){
@@ -61,5 +81,6 @@ public class StoryManager : MonoBehaviour {
 			line = pair.Key + ":" + pair.Value;
 			writer.WriteLine (line);
 		}
+        writer.Close();
 	}
 }
