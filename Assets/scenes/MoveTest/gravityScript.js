8d55cf22659bca92f271893113f178b62179e0fb
Matt Brahe <mattbrahe@gmail.com>
2016-10-11 22:13:40 -0400

    Testing new kinds of movement

---
+++ Assets/scenes/MoveTest/gravityScript.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+public var gravityStrength :float = 2;
+
+function Start () {
+	var rb = GetComponent(Rigidbody2D); 
+}
+
+function Update () {
+	var rb = GetComponent(Rigidbody2D); 
+
+	var i :int;
+	var gravityList = GameObject.FindGameObjectsWithTag("Gravity");
+	var gravity :Vector2;
+	var difference :Vector2;
+	var angle :float;
+	for (i = 0; i < gravityList.length; i++) {
+		gravity = Vector2(1, 0);
+		difference = gravityList[i].GetComponent(Rigidbody2D).position - rb.position;
+		
+		if (difference.magnitude != 0) {
+			gravity.x /= Mathf.Pow(difference.magnitude, 2);
+			gravity.x *= gravityList[i].GetComponent(Rigidbody2D).mass;
+			gravity.x *= gravityStrength * Time.deltaTime * rb.mass;
+	
+			angle = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+	
+			gravity = Quaternion.Euler(0, 0, angle) * gravity;
+	
+			rb.AddForce(gravity);
+		}
+	}
+}
---
+++ Assets/scenes/MoveTest/newMoveController.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class newMoveController : MonoBehaviour {
+
+	public float moveAmount = 20f;
+	public float maxSpeed = 5f;
+	public float friction = .4f;
+	public float movingFriction = .2f;
+	public float stunPower = 4;
+	public float turnAcceleration = 40f;
+	public float blastPower = 5;
+
+	private bool blast;
+
+	bool stunned;
+
+	Rigidbody2D rigidBody;
+
+	StunController playerStun;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D>();
+		playerStun = GetComponent<StunController>();
+		blast = false;
+
+		stunned = false;
+	}
+
+	void FixedUpdate() {
+		Vector2 inputDirection = new Vector2 (Input.GetAxis ("Horizontal"),
+			                         Input.GetAxis ("Vertical"));
+		inputDirection = inputDirection.normalized;
+		
+		bool noMove = Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0;
+
+		Vector2 acceleration = Vector2.zero;
+
+		if (rigidBody.velocity.magnitude != 0 && inputDirection.x != 0 && !blast) {
+			float theta = Mathf.Atan2 (rigidBody.velocity.y, rigidBody.velocity.x);
+			theta += -.04f * inputDirection.x;
+			Vector2 destVec = new Vector2 (Mathf.Cos (theta), Mathf.Sin (theta));
+			acceleration = -Vector3.Cross (rigidBody.velocity,
+				Vector3.Cross (rigidBody.velocity, destVec));
+			
+			acceleration = acceleration.normalized * turnAcceleration;
+		}
+
+		if (inputDirection.y != 0  && !blast) {
+			Debug.Log ("hello");
+			if (rigidBody.velocity.magnitude == 0) {
+				float angle = transform.eulerAngles.z;
+				Vector2 addition = new Vector2 (Mathf.Cos (angle), Mathf.Sin (angle));
+				addition *= moveAmount;
+				acceleration += addition;
+			} else {
+				acceleration += rigidBody.velocity.normalized * moveAmount;
+			}
+		}
+
+		if (blast) {
+			if (rigidBody.velocity.magnitude == 0) {
+				float angle = transform.eulerAngles.z;
+				Vector2 addition = new Vector2 (Mathf.Cos (angle), Mathf.Sin (angle));
+				addition *= blastPower;
+				acceleration += addition;
+			} else {
+				acceleration += rigidBody.velocity.normalized * blastPower;
+			}
+		}
+
+		rigidBody.AddForce (acceleration);
+		if (!noMove) {
+			rigidBody.AddForce (-1 * rigidBody.velocity * movingFriction);
+		}
+		else {
+			rigidBody.AddForce(-1 * rigidBody.velocity * friction);
+		}
+
+		if (playerStun.isStunned() == true && stunned == false)
+		{
+			rigidBody.velocity = playerStun.StunVector() * stunPower;
+			stunned = true;
+		}
+		else if (playerStun.isStunned() == false && stunned == true)
+		{
+			stunned = false;
+		}
+
+		Debug.Log ("acceleration: " + acceleration);
+		Debug.Log ("velocity: " + rigidBody.velocity);
+		Debug.Log ("input direction: " + inputDirection);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		blast = Input.GetButton ("Fire1");
+	}
+}
