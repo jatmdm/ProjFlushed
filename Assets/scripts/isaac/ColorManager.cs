2c1661d94d01ddf7359ec1c4fdcbedc0dff54d16
omegadude1 <isaac.t.thompson@gmail.com>
2016-11-04 15:10:05 -0400

    Modify StateManager, add two trigger areas, create script to change npc's color.
    testNPC
    -remove collider on suboject
    
    stateManager
    -interfaces with storyManager to store state in storyManager's dictionary
    -public fields for npc name, storymanager, and initalState
    -use getState() to get the current state
    -use changeState(int newState) to change npc's state to newState
    
    ChangeState trigger
    -when entered changes state between two states
    
    CheckState
    -when entered prints state to log area
    
    ColorManager
    -changes color based on npc state as proof of concept

---
+++ Assets/scripts/isaac/ColorManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorManager : MonoBehaviour {
+
+    public StateManager stateManager;
+    public SpriteRenderer spriteRenderer;
+    public int lastState;
+
+	// Use this for initialization
+	void Start () {
+        stateManager = GetComponentInChildren<StateManager>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        lastState = stateManager.getState();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int newState = stateManager.getState();
+        if (newState != lastState)
+        {
+            changeColor(newState);
+            lastState = newState;
+        }
+	}
+
+    private void changeColor (int state)
+    {
+        if (state == 0)
+        {
+            spriteRenderer.color = Color.green;
+        }
+        else if (state == 1)
+        {
+            spriteRenderer.color = Color.red;
+        }
+    }
+}
--- Assets/scripts/isaac/StateManager.cs
+++ Assets/scripts/isaac/StateManager.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class StateManager : MonoBehaviour {
 
+    public string npcName;
+    public StoryManager storyManager;
+    public int initialState;
+
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+        storyManager.storyConditions.Add(npcName, initialState);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public int getState ()
+    {
+        int state = (int)storyManager.storyConditions[npcName];
+        return state;
+    }
+
+    public void changeState (int newState)
+    {
+        storyManager.ChangeConditionState(npcName, newState);
+    }
 }
