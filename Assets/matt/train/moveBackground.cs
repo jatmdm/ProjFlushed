1b589e3d46d77a4c85c9cec726e9fc2a4d527123
Matt Brahe <mattbrahe@gmail.com>
2016-11-21 18:35:58 -0500

    oh yes!

---
+++ Assets/matt/train/moveBackground.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveBackground : MonoBehaviour {
+
+	public float velocity = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = transform.position;
+		newPosition.x -= velocity * Time.deltaTime;
+		transform.position = newPosition;
+	}
+}
